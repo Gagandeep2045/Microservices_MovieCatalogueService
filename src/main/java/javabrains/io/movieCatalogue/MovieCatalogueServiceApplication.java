789834ee99c95ce923c01e532c71f8b3ec2d1753
Gagan <gagandeep.singh2045@gmail.com>
2019-10-29 06:10:27 +0530

    Vid9:Timeout for RestTemplate

--- src/main/java/javabrains/io/movieCatalogue/MovieCatalogueServiceApplication.java
+++ src/main/java/javabrains/io/movieCatalogue/MovieCatalogueServiceApplication.java
@@ -1,13 +1,12 @@
 package javabrains.io.movieCatalogue;
 
 import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-
 import javabrains.io.movieCatalogue.interceptor.MovieCatalogueInterceptor;
 
 @SpringBootApplication
@@ -20,7 +19,9 @@ public class MovieCatalogueServiceApplication {
 	@Bean
 	@LoadBalanced
 	RestTemplate getRestTemplate() {
-		RestTemplate r = new RestTemplate();
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		clientHttpRequestFactory.setConnectTimeout(6000);//generic timeout of 6 seconds for this restTemplate
+		RestTemplate r = new RestTemplate(clientHttpRequestFactory);
 		r.setInterceptors(Arrays.asList(new MovieCatalogueInterceptor()));
 		return r;
 	}
